a46bb89d5f42aaf031ad89a61d3c06420de7d54d
Patrick <seitzpr@gmail.com>
2023-08-14 14:42:43 -0400

    Added pdf to text converter

--- space.py
+++ space.py
@@ -1,18 +1,16 @@
 import sys
 from pypdf import PdfReader
 
-input = sys.argv[1]
-print(f'You said "{input}"')
+# input = sys.argv[1]
+# print(f'You said "{input}"')
 
 pdf = open("Storage_Rep_2023-08-10.pdf", "rb")
 reader = PdfReader(pdf)
-number_pages = len(reader.pages)
 
-print(f"Number of pages: {number_pages}")
-
-page0 = reader.pages[0]
-page1 = reader.pages[1]
-page0text = page0.extract_text()
-page1text = page1.extract_text()
-print(page0text)
-print(page1text)
+outputfile = open("./storage_output.txt", "a")
+count = 0
+for i in reader.pages:
+    page = reader.pages[count]
+    text = page.extract_text()
+    outputfile.writelines(text + "\n")
+    count = count + 1
