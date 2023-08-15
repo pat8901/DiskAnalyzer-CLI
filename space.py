538122b89b66e0551644cc7be6598808e84568f8
Patrick <seitzpr@gmail.com>
2023-08-15 14:00:59 -0400

    Added csv functions

--- space.py
+++ space.py
@@ -1,14 +1,21 @@
 import sys
 from pypdf import PdfReader
+import csv
 
 
 def main():
     # input = sys.argv[1]
     # print(f'You said "{input}"')
     createFullOutput()
-    createResearchOutput()
+    # createResearchOutput()
+    createResearchOutputTest()
+    createDepartmentOutput()
+    createCollegesOutput()
     # convertToCSV()
-    convertToCSVTest()
+    # convertToCSVTest()
+    listLine()
+    convert()
+    csvWriter()
 
 
 def createFullOutput():
@@ -38,24 +45,98 @@ def createResearchOutput():
                     f_output.write(line + "\n")
 
 
-def convertToCSV():
-    with open("groups/research.txt", "r") as f_input:
-        with open("temp/test.txt", "w") as f_output:
+def createResearchOutputTest():
+    begin = "-------------------------------   -------    ------------    ------------    ------------    --------------"
+    end = "=========================================|===============|===============|===============|=================|"
+    beginFound = False
+    with open("temp/full_output.txt", "r") as f_input:
+        with open("groups/research_test.txt", "w") as f_output:
             for line in f_input:
-                line = line.strip()
-                " ".join(line.split())
-                f_output.write(line + "\n")
+                if end in line:
+                    break
+                if beginFound:
+                    line = line.strip()
+                    f_output.write(line + "\n")
+                elif begin in line:
+                    beginFound = True
 
 
-def convertToCSVTest():
-    with open("groups/research.txt", "r") as f_input:
-        with open("temp/test.txt", "w") as f_output:
-            content = f_input.readlines()
-            # print(content[5])
-            # for line in f_input:
-            #     line = line.strip()
-            #     " ".join(line.split())
-            f_output.write(content[4])
+def createDepartmentOutput():
+    header = "                                            Space Used by Departments"
+    begin = "-----------------------------------------    ------------    ------------    ------------    --------------"
+    end = "=========================================|===============|===============|===============|=================|"
+    headerFound = False
+    beginFound = False
+    with open("temp/full_output.txt", "r") as f_input:
+        with open("groups/departments.txt", "w") as f_output:
+            for line in f_input:
+                if header in line:
+                    headerFound = True
+                if headerFound:
+                    if end in line:
+                        break
+                    elif beginFound:
+                        line = line.strip()
+                        f_output.write(line + "\n")
+                    elif begin in line:
+                        beginFound = True
+
+
+def createCollegesOutput():
+    header = "                                            Space Used by Colleges"
+    begin = "-----------------------------------------    ------------    ------------    ------------    --------------"
+    end = "=========================================|===============|===============|===============|=================|"
+    headerFound = False
+    beginFound = False
+    with open("temp/full_output.txt", "r") as f_input:
+        with open("groups/colleges.txt", "w") as f_output:
+            for line in f_input:
+                if header in line:
+                    headerFound = True
+                if headerFound:
+                    if end in line:
+                        break
+                    elif beginFound:
+                        line = line.strip()
+                        f_output.write(line + "\n")
+                    elif begin in line:
+                        beginFound = True
+
+
+def listLine():
+    with open("groups/research_test.txt", "r") as f:
+        with open("temp/test.txt", "w") as f_out:
+            content = f.readlines()
+            line = content[0]
+            line = line.split("|")
+            for i in line:
+                string = i.strip()
+                # string = i.replace(" ", ",")
+                print(string)
+                f_out.write(string + ",")
+
+
+# Best working to csv so far
+def convert():
+    with open("groups/research_test.txt", "r") as f:
+        with open("temp/test_full.txt", "w") as f_out:
+            for line in f:
+                line = line.split("|")
+                for word in line:
+                    string = word.strip()
+                    # string = i.replace(" ", ",")
+                    print(string)
+                    f_out.write(string + ",")
+                # f_out.write("\n")
+
+
+def csvWriter():
+    with open("groups/research_test.txt", "r") as f:
+        with open("temp/test.csv", "w", newline="") as file:
+            for line in f:
+                line = line.split("|")
+                writer = csv.writer(file)
+                writer.writerow(line)
 
 
 def getTotalStorage():
