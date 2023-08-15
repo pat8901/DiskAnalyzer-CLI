c82c059128b983c726788f9a769cc5a98f8b64e7
Patrick <seitzpr@gmail.com>
2023-08-15 11:52:18 -0400

    Created functions and cleaned code

--- space.py
+++ space.py
@@ -1,33 +1,66 @@
 import sys
 from pypdf import PdfReader
-import io
 
-# input = sys.argv[1]
-# print(f'You said "{input}"')
 
-pdf = open("Storage_Rep_2023-08-10.pdf", "rb")
-reader = PdfReader(pdf)
+def main():
+    # input = sys.argv[1]
+    # print(f'You said "{input}"')
+    createFullOutput()
+    createResearchOutput()
+    # convertToCSV()
+    convertToCSVTest()
 
-outputfile = open("temp/full_output.txt", "a")
-count = 0
-for i in reader.pages:
-    page = reader.pages[count]
-    text = page.extract_text()
-    outputfile.writelines(text + "\n")
-    count = count + 1
 
-search_string = "Space Used by Departments"
-# search_string = "M EDT 20"
-research_output = open("groups/research.txt", "a")
-with io.open("temp/full_output.txt", "r") as f:
-    for line in f:
-        line = line.strip()
-        if search_string == line:
-            print(f'Found the search string "{line}"')
-            break
-        else:
-            research_output.writelines(line + "\n")
+def createFullOutput():
+    pdf = open("Storage_Rep_2023-08-10.pdf", "rb")
+    reader = PdfReader(pdf)
+    with open("temp/full_output.txt", "w") as f_output:
+        # outputfile = open("temp/full_output.txt", "a")
+        count = 0
+        for i in reader.pages:
+            page = reader.pages[count]
+            text = page.extract_text()
+            f_output.write(text + "\n")
+            count = count + 1
+
+
+def createResearchOutput():
+    search_string = "Space Used by Departments"
+    # search_string = "M EDT 20"
+    with open("temp/full_output.txt", "r") as f_input:
+        with open("groups/research.txt", "w") as f_output:
+            for line in f_input:
+                line = line.strip()
+                if search_string in line:
+                    print(f'Found the search string "{line}"')
+                    break
+                else:
+                    f_output.write(line + "\n")
+
+
+def convertToCSV():
+    with open("groups/research.txt", "r") as f_input:
+        with open("temp/test.txt", "w") as f_output:
+            for line in f_input:
+                line = line.strip()
+                " ".join(line.split())
+                f_output.write(line + "\n")
+
+
+def convertToCSVTest():
+    with open("groups/research.txt", "r") as f_input:
+        with open("temp/test.txt", "w") as f_output:
+            content = f_input.readlines()
+            # print(content[5])
+            # for line in f_input:
+            #     line = line.strip()
+            #     " ".join(line.split())
+            f_output.write(content[4])
 
 
 def getTotalStorage():
     print("test")
+
+
+if __name__ == "__main__":
+    main()
