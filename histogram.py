a50aab6187d71b000db56d17a64d90bb549ebb50
Patrick <seitzpr@gmail.com>
2023-08-28 12:51:50 -0400

    Added updated histogram labels for scott

--- histogram.py
+++ histogram.py
@@ -19,7 +19,29 @@ import misc
 def getGroupHistogram(input, column, date):
     df = pd.read_csv(f"csv/{input}_{date}.csv")
     terabyte = 1000000000
-    bins = [0, 1, 50, 100, 200, 300, 400, 500, 600]
+    xaxis = 0
+
+    scott_bins = [0, 0.1, 0.5, 1, 2, 10, 20, 50, 10000000]
+    scott_labels = [
+        "0-100GB",
+        "100-500GB",
+        "500GB-1TB",
+        "1-2TB",
+        "2-10TB",
+        "10-20TB",
+        "20-50TB",
+        "50-?TB",
+    ]
+
+    bin0 = [0, 0.1]
+    label0 = ["0-100GB"]
+    bin1 = [0.5, 1]
+    label1 = ["500GB-1TB"]
+    bin2 = [2, 10]
+    label2 = ["2-10TB"]
+    bin3 = [20, 50, 10000000]
+    label3 = ["20-50TB", "50-?TB"]
+
     better_bins = tools.binCreator(80, 2, 0)
     better_bins_labels = tools.binLabelCreator(80, 2, 0)
     fig, ax = plt.subplots()
@@ -28,12 +50,30 @@ def getGroupHistogram(input, column, date):
     df[f"{column}"] = df[f"{column}"].div(terabyte)
 
     # Creates a new column in dataframe with each row having their appropriate bin
-    df["bin"] = pd.cut(
-        df[f"{column}"], better_bins, labels=better_bins_labels, right=False
-    )
+    # df["bin"] = pd.cut(
+    #     df[f"{column}"], better_bins, labels=better_bins_labels, right=False
+    # )
+
+    df["bin"] = pd.cut(df[f"{column}"], scott_bins, labels=scott_labels, right=False)
+
+    df["bin0"] = pd.cut(df[f"{column}"], bin0, labels=label0, right=False)
+    df["bin1"] = pd.cut(df[f"{column}"], bin1, labels=label1, right=False)
+    df["bin2"] = pd.cut(df[f"{column}"], bin2, labels=label2, right=False)
+    df["bin3"] = pd.cut(df[f"{column}"], bin3, labels=label3, right=False)
 
     # Counts the frequency of counts within bin ranges
     data = df["bin"].value_counts(sort=False)
+    data0 = df["bin0"].value_counts(sort=False)
+    data1 = df["bin1"].value_counts(sort=False)
+    data2 = df["bin2"].value_counts(sort=False)
+    data3 = df["bin3"].value_counts(sort=False)
+    print(data0)
+    print(data1)
+    print(data2)
+    print(data3)
+    print(data)
+    # with pd.option_context("display.max_rows", None, "display.max_columns", 10):
+    #     print(df)
 
     # *Debugging* Shows full table data
     # with pd.option_context("display.max_rows", None, "display.max_columns", None):
@@ -42,20 +82,41 @@ def getGroupHistogram(input, column, date):
     # Setting the graph's x/y labels and title
     ax.set(
         ylabel="Frequency",
-        xlabel="Terabytes",
         title=f"{column} - Number of Users Within a Range of Storage {date}",
     )
 
     # Creating bar plots and adding them to graph
+    # for i in range(len(data)):
+    #     p = ax.bar(better_bins_labels[i], data[i], label=better_bins_labels[i], width=1)
+    #     ax.bar_label(p, label_type="edge")
+
+    # Creating bar plots and adding them to graph
     for i in range(len(data)):
-        p = ax.bar(better_bins_labels[i], data[i], label=better_bins_labels[i], width=1)
-        ax.bar_label(p, label_type="edge")
+        if i == 1:
+            continue
+        if i == 3:
+            continue
+        if i == 5:
+            continue
+        else:
+            p = ax.bar(scott_labels[i], data[i], label=scott_labels[i], width=1)
+            ax.bar_label(p, label_type="edge")
+
+    # p0 = ax.bar(label0, data0, label=label0, width=1)
+    # ax.bar_label(p0, label_type="edge")
+    # p1 = ax.bar(label1, data1, label=label1, width=1)
+    # ax.bar_label(p1, label_type="edge")
+    # p2 = ax.bar(label2, data2, label=label2, width=1)
+    # ax.bar_label(p2, label_type="edge")
+    # p3 = ax.bar(label3, data3, label=label3, width=1)
+    # ax.bar_label(p3, label_type="edge")
 
     # Legend - Not really needed
     # lgd = ax.legend(bbox_to_anchor=(1.05, 1), loc="upper left")
 
     # Rotating x labels 90 degrees
-    plt.xticks(rotation=90, ha="right")
+    # plt.xticks(rotation=90, ha="right")
+    plt.xticks(rotation=0, ha="center")
 
     # Setting the size of the graph for when it will be saved to a file
     figure = plt.gcf()
@@ -63,7 +124,7 @@ def getGroupHistogram(input, column, date):
 
     # Saving the figure
     plt.savefig(
-        f"graphs/research/group/histogram_{column}_{date}.pdf",
+        f"graphs/research/group/test_histogram_{column}_{date}.pdf",
         dpi=300,
         format="pdf",
         # bbox_extra_artists=(lgd,),
--- space.py
+++ space.py
@@ -14,33 +14,34 @@ import histogram
 
 
 def main():
-    file_input = sys.argv[1]
-    print(f'You said "{file_input}"')
-    
-    print("Which group would you like to create graphs for?")
-    selection_group= input(
-    """
-    Choose a number
+    # file_input = sys.argv[1]
+    # print(f'You said "{file_input}"')
 
-    Options:
-    [1] Researchers
-    [2] Departments  
-    [3] Colleges
-    """)
+    # print("Which group would you like to create graphs for?")
+    # selection_group = input(
+    #     """
+    # Choose a number
 
-    match selection_group:
-        case "1":
-            print('you choose researchers')
-            getJob()
+    # Options:
+    # [1] Researchers
+    # [2] Departments
+    # [3] Colleges
+    # """
+    # )
 
-        case '2':
-            print('you choose departments')
-            getJob()
-        case '3':
-            print('you choose colleges')
-            getJob()
-        case _:
-            print('you choose invalid option!')
+    # match selection_group:
+    #     case "1":
+    #         print("you choose researchers")
+    #         getJob()
+
+    #     case "2":
+    #         print("you choose departments")
+    #         getJob()
+    #     case "3":
+    #         print("you choose colleges")
+    #         getJob()
+    #     case _:
+    #         print("you choose invalid option!")
 
     report_date = tools.getReportDate("reports/Storage_Rep_2023-08-10.pdf")
 
@@ -53,60 +54,58 @@ def main():
     writer.csvWriter("departments", "departments", report_date)
     writer.csvWriter("colleges", "colleges", report_date)
 
-    # getUserBarChart("research")
-    # test_getUserBarChart("research")
-    #bar.getUserBarChart("research", report_date)
-
-   # histogram.getGroupHistogram("research", "AFS Groups", report_date)
-    # secGetGroupHistogram('research','AFS Groups')
+    histogram.getGroupHistogram("research", "AFS Groups", report_date)
 
     # ===================================================================
     # Testing to see if I can use different pdfs to create data
-    report_date_2 = tools.getReportDate("reports/Storage_Rep_2022-08-01.pdf")
-    writer.createFullOutput("reports/Storage_Rep_2022-08-01.pdf")
-    writer.createResearchOutput(report_date_2)
-    writer.createDepartmentOutput(report_date_2)
-    writer.createCollegesOutput(report_date_2)
-    writer.csvWriter("research", "research", report_date_2)
-    writer.csvWriter("departments", "departments", report_date_2)
-    writer.csvWriter("colleges", "colleges", report_date_2)
-   # histogram.getGroupHistogram("research", "AFS Groups", report_date_2)
+    # report_date_2 = tools.getReportDate("reports/Storage_Rep_2022-08-01.pdf")
+    # writer.createFullOutput("reports/Storage_Rep_2022-08-01.pdf")
+    # writer.createResearchOutput(report_date_2)
+    # writer.createDepartmentOutput(report_date_2)
+    # writer.createCollegesOutput(report_date_2)
+    # writer.csvWriter("research", "research", report_date_2)
+    # writer.csvWriter("departments", "departments", report_date_2)
+    # writer.csvWriter("colleges", "colleges", report_date_2)
+    # # histogram.getGroupHistogram("research", "AFS Groups", report_date_2)
 
-    # Testing to see if I can use different pdfs to create data
-    report_date_3 = tools.getReportDate("reports/Storage_Rep_2021-08-01.pdf")
-    writer.createFullOutput("reports/Storage_Rep_2021-08-01.pdf")
-    writer.createResearchOutput(report_date_3)
-    writer.createDepartmentOutput(report_date_3)
-    writer.createCollegesOutput(report_date_3)
-    writer.csvWriter("research", "research", report_date_3)
-    writer.csvWriter("departments", "departments", report_date_3)
-    writer.csvWriter("colleges", "colleges", report_date_3)
-    #histogram.getGroupHistogram("research", "AFS Groups", report_date_3)
-    
-    #bar.testBatchGetUserBarChart("research", report_date)
-    #bar.hellogetUserBarChart("research", report_date)
-    
+    # # Testing to see if I can use different pdfs to create data
+    # report_date_3 = tools.getReportDate("reports/Storage_Rep_2021-08-01.pdf")
+    # writer.createFullOutput("reports/Storage_Rep_2021-08-01.pdf")
+    # writer.createResearchOutput(report_date_3)
+    # writer.createDepartmentOutput(report_date_3)
+    # writer.createCollegesOutput(report_date_3)
+    # writer.csvWriter("research", "research", report_date_3)
+    # writer.csvWriter("departments", "departments", report_date_3)
+    # writer.csvWriter("colleges", "colleges", report_date_3)
+    # # histogram.getGroupHistogram("research", "AFS Groups", report_date_3)
+
+    # # bar.testBatchGetUserBarChart("research", report_date)
+    # # bar.hellogetUserBarChart("research", report_date)
+
+
 def getJob():
     print("Which job would you like to preform?")
-    selection_job= input(
-    """
+    selection_job = input(
+        """
     Choose a number
 
     Options:
     [1] Create a batch of graphs for individual users
     [2] Create a histogram on the frequencies of ranges for the whole group of users  
     [3] Do nothing
-    """)
+    """
+    )
     match selection_job:
         case "1":
-            print('you choose create batch of graphs')
+            print("you choose create batch of graphs")
 
-        case '2':
-            print('you choose create a histogram')
-        case '3':
-            print('you choose to do nothing')
+        case "2":
+            print("you choose create a histogram")
+        case "3":
+            print("you choose to do nothing")
         case _:
-            print('you choose invalid option!')
+            print("you choose invalid option!")
+
 
 if __name__ == "__main__":
     main()
