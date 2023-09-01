54dea77074027d5cd15f87f4146b03abc3163df3
Patrick <seitzpr@gmail.com>
2023-09-01 11:59:52 -0400

    Added some flask components

--- app.py
+++ app.py
@@ -1,8 +1,38 @@
 from flask import Flask
+import pandas as pd
+import csv
+import base64
+from io import BytesIO
+from matplotlib.figure import Figure
+import histogram
+import tools
+import flask_histogram
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def root():
+    df = pd.read_csv(f"csv/research_2023-08-10.csv")
+    json_data = df.to_json(orient="values")
+    return json_data
+
+
+@app.route("/hello")
+def hello():
+    # Generate the figure **without using pyplot**.
+    fig = Figure()
+    ax = fig.subplots()
+    ax.plot([1, 2])
+    # Save it to a temporary buffer.
+    buf = BytesIO()
+    fig.savefig(buf, format="png")
+    # Embed the result in the html output.
+    data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    return f"<img src='data:image/png;base64,{data}'/>"
+
+
+@app.route("/hello/world")
+def world():
+    data = flask_histogram.convertToImage()
+    return f"<img src='data:image/png;base64,{data}'/>"
--- flask_histogram.py
+++ flask_histogram.py
@@ -2,114 +2,10 @@ import pandas as pd
 import matplotlib
 import matplotlib.pyplot as plt
 import tools
+import base64
+import io
 
 
-# +======================================================================================+
-# |  Creates a histogram using binned data as x values and their frequencies as y values |
-# |             *bins are auto-made using the hist function instead of bar*              |
-# +======================================================================================+
-def getGroupHistogram(group, column, date):
-    df = pd.read_csv(f"csv/{group}_{date}.csv")
-    terabyte = 1000000000
-    scott_bins = [0, 0.001, 0.1, 0.5, 1, 4, 10, 20, 50, 10000000]
-    scott_labels = [
-        "0-1 GB",
-        "1-100 GB",
-        "100-500 GB",
-        "500-1 TB",
-        "1-4 TB",
-        "4-10 TB",
-        "10-20 TB",
-        "20-50 TB",
-        "> 50 TB",
-    ]
-
-    better_bins = tools.binCreator(80, 2, 0)
-    better_bins_labels = tools.binLabelCreator(80, 2, 0)
-    fig, ax = plt.subplots()
-
-    # Covert data in column from kilobytes to terabytes
-    df[f"{column}"] = df[f"{column}"].div(terabyte)
-
-    # Creates a new column in dataframe with each row having their appropriate bin
-    # df["bin"] = pd.cut(
-    #     df[f"{column}"], better_bins, labels=better_bins_labels, right=False
-    # )
-
-    df["bin"] = pd.cut(df[f"{column}"], scott_bins, labels=scott_labels, right=False)
-
-    # Counts the frequency of counts within bin ranges
-    data = df["bin"].value_counts(sort=False)
-    print(data)
-
-    # *Debugging* Shows full table data
-    with pd.option_context("display.max_rows", None, "display.max_columns", None):
-        print(df)
-
-    # Setting the graph's x/y labels and title
-    ax.set_ylabel("Number of Users", fontsize=16)
-    if column == "Tot.Used Space":
-        ax.set_title(f"Total Used Space ({date})", fontsize=16)
-    else:
-        ax.set_title(f"{column} ({date})", fontsize=16)
-    matplotlib.pyplot.xticks(fontsize=12)
-    matplotlib.pyplot.yticks(fontsize=14)
-    plt.figtext(
-        0.12,
-        0.01,
-        f"*{data[0]} users between 0GB-1GB not displayed",
-        horizontalalignment="left",
-        fontsize=12,
-    )
-    # ax.grid(axis='y')
-
-    # Creating bar plots and adding them to graph
-    # for i in range(len(data)):
-    #     p = ax.bar(better_bins_labels[i], data[i], label=better_bins_lab1els[i], width=1)
-    #     ax.bar_label(p, label_type="edge")
-
-    # Creating bar plots and adding them to graph
-    for i in range(len(data)):
-        if i == 0:
-            continue
-        else:
-            p = ax.bar(
-                scott_labels[i],
-                data[i],
-                label=scott_labels[i],
-                width=1,
-                edgecolor="white",
-            )
-
-            ax.bar_label(p, label_type="edge", fontsize=16)
-
-    # Legend - Not really needed
-    # lgd = ax.legend(bbox_to_anchor=(1.05, 1), loc="upper left")
-
-    # Rotating x labels 90 degrees
-    # plt.xticks(rotation=90, ha="right")
-    plt.xticks(rotation=0, ha="center")
-
-    # Setting the size of the graph for when it will be saved to a file
-    figure = plt.gcf()
-    figure.set_size_inches(24, 8.5)
-
-    # Saving the figure
-    plt.savefig(
-        f"graphs/research/group_reports/{group}_{column}_histogram_{date}.pdf",
-        dpi=300,
-        format="pdf",
-        # bbox_extra_artists=(lgd,),
-        bbox_inches="tight",
-    )
-
-    plt.show()
-
-
-# +======================================================================================+
-# |  Creates a histogram using binned data as x values and their frequencies as y values |
-# |             *bins are auto-made using the hist function instead of bar*              |
-# +======================================================================================+
 def getStackedGroupHistogram(group, date):
     df = pd.read_csv(f"csv/{group}_{date}.csv")
     row_count = len(df.index)
@@ -124,8 +20,7 @@ def getStackedGroupHistogram(group, date):
         "tab:gray",
         "tab:brown",
     ]
-    better_bins = tools.binCreator(80, 2, 0)
-    better_bins_labels = tools.binLabelCreator(80, 2, 0)
+
     scott_unused_bins = [0, 0.001]
     scott_unused_labels = ["0GB-1GB"]
     scott_bins = [0.001, 0.1, 0.5, 1, 4, 10, 20, 50, 10000000]
@@ -147,11 +42,6 @@ def getStackedGroupHistogram(group, date):
     df["Users AFS"] = df["Users AFS"].div(terabyte)
     df["Users Panas."] = df["Users Panas."].div(terabyte)
     df["Tot.Used Space"] = df["Tot.Used Space"].div(terabyte)
-
-    # Creates a new column in dataframe with each row having their appropriate bin
-    # df["bin"] = pd.cut(
-    #     df[f"{column}"], better_bins, labels=better_bins_labels, right=False
-    # )
 
     df["bin_AFS_Groups"] = pd.cut(
         df["AFS Groups"], scott_bins, labels=scott_labels, right=False
@@ -191,21 +81,12 @@ def getStackedGroupHistogram(group, date):
     data_Users_Panas_unused = df["bin_Users_Panas._unused"].value_counts(sort=False)
     data_total_unused = df["bin_total_unused"].value_counts(sort=False)
 
-    # *Debugging* Shows full table data
-    # with pd.option_context("display.max_rows", None, "display.max_columns", None):
-    #     print(df)
-
     # Setting the graph's x/y labels and title
     ax.set_ylabel("Number of Users", fontsize=16)
     ax.set_title(
         f"Combined Counts From AFS Groups, Users AFS, and Users Panasas ({date})",
         fontsize=16,
     )
-
-    # Creating bar plots and adding them to graph
-    # for i in range(len(data)):
-    #     p = ax.bar(better_bins_labels[i], data[i], label=better_bins_labels[i], width=1)
-    #     ax.bar_label(p, label_type="edge")
 
     # Creating bar plots and adding them to graph
     for i in range(len(data_AFS_Groups)):
@@ -255,16 +136,6 @@ def getStackedGroupHistogram(group, date):
             color="black",
         )
 
-    # Legend - Not really needed
-    # lgd = ax.legend(bbox_to_anchor=(1.05, 1), loc="upper left")
-    # lgd = ax.legend(
-    #     [p2, p1, p0],
-    #     ["Users Panasas", "Users AFS", "AFS Groups"],
-    #     bbox_to_anchor=(0.85, 0.85),
-    #     loc="upper left",
-    #     fontsize=18,
-    # )
-
     ax.set_axisbelow(True)
 
     plt.grid(axis="y", color="0.95", zorder=0)
@@ -296,22 +167,32 @@ def getStackedGroupHistogram(group, date):
         zorder=3,
     )
 
-    # Rotating x labels 90 degrees
-    # plt.xticks(rotation=90, ha="right")
-    # plt.yticks(np.arange(0, 195, 5))
     plt.xticks(rotation=0, ha="center")
 
     # Setting the size of the graph for when it will be saved to a file
     figure = plt.gcf()
-    figure.set_size_inches(24, 8.5)
+    figure.set_size_inches(18, 8.5)
 
     # Saving the figure
-    plt.savefig(
-        f"graphs/research/group_reports/{group}_combined_histogram_{date}.pdf",
-        dpi=300,
-        format="pdf",
-        # bbox_extra_artists=(lgd,),
-        bbox_inches="tight",
-    )
+    # plt.savefig(
+    #     f"graphs/research/group_reports/{group}_combined_histogram_{date}.pdf",
+    #     dpi=300,
+    #     format="pdf",
+    #     bbox_inches="tight",
+    # )
 
-    plt.show()
+
+def convertToImage():
+    report_date = tools.getReportDate("reports/Storage_Rep_2023-08-10.pdf")
+    getStackedGroupHistogram("research", report_date)
+
+    # Save plot to a BytesIO object
+    img = io.BytesIO()
+    plt.savefig(img, format="png", bbox_inches="tight")
+    img.seek(0)
+
+    # Convert BytesIO object to base64 string
+    img_b64 = base64.b64encode(img.getvalue()).decode()
+    plt.close()
+
+    return img_b64
